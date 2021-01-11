4e397ea1971a583eaa9c37ecdca5280b4cf4782f
ks289 <ks289@st-andrews.ac.uk>
2021-01-11 11:26:46 +0000

    complete

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        This is some text about us. This is some text about us. This is some text about us.
+        This is some text about us. This is some text about us. This is some text about us.
+        This is some text about us. This is some text about us. This is some text about us.
+        This is some text about us. This is some text about us. This is some text about us.
+        This is some text about us. This is some text about us. This is some text about us.
+        This is some text about us. This is some text about us. This is some text about us.
+      </p>
+    </section>
+  );
+}
+
+export default About
---
+++ src/pages/Error.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>oops! Wrong turn</h1>
+        <Link to="/" className="btn btn-primary">
+          return home
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default Error
